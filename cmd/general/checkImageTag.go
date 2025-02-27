60403fbb0fa6ccc807f814918bff76421e1656d4
BcGee <blancspace@naver.com>
2025-02-27 17:03:26 +0900

    check image tag
    이미지 태그에서 Latest 가 있는지 확인하게 작성

---
+++ cmd/general/checkImageTag.go
@@ -0,0 +1,35 @@
+package general
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func CheckImageTag(client kubernetes.Interface) bool {
+	// 모든 네임스페이스에서 파드를 리스트
+	pods, err := client.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 모든 파드의 컨테이너 이미지를 확인
+	for _, pod := range pods.Items {
+		for _, container := range pod.Spec.Containers {
+			// 이미지에 "latest"가 포함되어 있는지 확인
+			if strings.Contains(container.Image, "latest") {
+				fmt.Println("FAIL: Latest Tag on Container Image Found")
+
+				return true
+			}
+		}
+	}
+
+	// "latest" 이미지가 없는 경우
+	fmt.Println("pass: Latest Tag on Container Image Not Found")
+
+	return false
+}
