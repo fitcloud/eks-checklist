06994123f00dea4a570fa43af0ec66b8c7f7e3ab
kira0916 <rjrjswm@gmail.com>
2025-03-12 06:38:35 +0000

    HPA 설정 사용 유무 확인

--- cmd/stability/horizontalPodAutoscaler.go
+++ cmd/stability/horizontalPodAutoscaler.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+// 클러스터에 Horizontal Pod Autoscaler가 설정정되어 있는지 확인
 func CheckHpa(client kubernetes.Interface) bool {
 	// 모든 Namespace Horizontal Pod Autoscaler 목록 확인인
 	hpas, err := client.AutoscalingV1().HorizontalPodAutoscalers(metav1.NamespaceAll).List(context.TODO(), v1.ListOptions{})
