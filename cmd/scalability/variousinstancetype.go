928b602f222ef5973cdcd542358a0993be3f437c
kira0916 <rjrjswm@gmail.com>
2025-03-12 05:27:54 +0000

    현재 사용중인 InstanceNodetype, fargate 확인 및 출력. 단일 타입 사용시 fail 처리

--- cmd/scalability/variousinstancetype.go
+++ cmd/scalability/variousinstancetype.go
@@ -2,6 +2,8 @@ package scalability
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -23,7 +25,20 @@ func CheckInstanceTypes(client kubernetes.Interface) bool {
 		if instanceType, exists := node.Labels["beta.kubernetes.io/instance-type"]; exists {
 			instanceTypes[instanceType] = true
 		}
+
+		// Fargate 작업인 경우, 인스턴스 유형으로 "fargate"를 추가
+		// ProviderID가 "fargate"인 경우를 확인
+		if node.Spec.ProviderID != "" && strings.Contains(node.Spec.ProviderID, "fargate") {
+			instanceTypes["fargate"] = true
+		}
 	}
+
+	// 인스턴스 유형들을 쉼표로 구분하여 출력
+	var types []string
+	for instanceType := range instanceTypes {
+		types = append(types, instanceType)
+	}
+	fmt.Printf("Instance Types used in the cluster: %s\n", strings.Join(types, ", "))
 
 	// 다양한 인스턴스를 사용하고 있는지 확인
 	return len(instanceTypes) > 1
