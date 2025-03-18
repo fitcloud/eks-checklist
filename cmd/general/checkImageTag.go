5712bc81b7298b490c2c9983079182df574402d2
BcGee <blancspace@naver.com>
2025-03-18 13:59:43 +0900

    removed wrong function

--- cmd/general/checkImageTag.go
+++ cmd/general/checkImageTag.go
@@ -40,25 +40,3 @@ func CheckImageTag(client kubernetes.Interface) bool {
 
 	return false
 }
-
-func BubbleSort(numbers []int) []int {
-	unsortedUntilIndex := len(numbers) - 1
-	swap := true
-
-	for swap {
-		swap = false
-		for i := 0; i < unsortedUntilIndex; i++ {
-			first := numbers[i]
-			second := numbers[i+1]
-
-			if first > second {
-				numbers[i] = second
-				numbers[i+1] = first
-				swap = true
-			}
-		}
-		unsortedUntilIndex--
-	}
-
-	return numbers
-}
