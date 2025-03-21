f09a9cfdc19f08022f342cb8aa1daa3ec3f72f8e
BcGee <blancspace@naver.com>
2025-03-21 15:01:34 +0900

    카펜터전용 노드그룹 파일 생성(테스트파일 미작성)

--- cmd/root.go
+++ cmd/root.go
@@ -91,6 +91,7 @@ var rootCmd = &cobra.Command{
 
 		// Scalability 항목 체크 기능은 하단 항목에 추가
 		fmt.Printf("\n===============[Stability Check]===============\n")
+		scalability.CheckNodeGroupUsage(k8sClient)
 
 		// 클러스터에 Cluster Autoscaler가 설치되어 있는지 확인
 		if stability.CheckClusterAutoscalerEnabled(k8sClient) {
---
+++ cmd/scalability/karpenter_nodegroup_test.go
@@ -0,0 +1,1 @@
+package scalability_test
