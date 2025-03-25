77508d7f43d5b23e506e634b99d388ab5b20396e
BcGee <blancspace@naver.com>
2025-03-25 10:17:17 +0900

    Testfile (#44)
    * 1차 커밋
    
    * 1차 커밋
    
    * modified:   .github/workflows/dev-pr.yaml
    	modified:   cmd/general/check_image_tag.go
    	modified:   cmd/general/check_image_tag_test.go
    	modified:   cmd/network/aws_loadbalancer_controller.go
    	modified:   cmd/network/aws_loadbalancer_pod_ip.go
    	modified:   cmd/network/check_vpc_cni_prefix_mode.go
    	modified:   cmd/network/check_vpc_cni_prefix_mode_test.go
    	modified:   cmd/network/endpointslices.go
    	modified:   cmd/network/endpointslices_test.go
    	modified:   cmd/network/kubeproxy_ipvs.go
    	modified:   cmd/network/kubeproxy_ipvs_test.go
    	modified:   cmd/network/readiness_gate.go
    	modified:   cmd/network/subnet_ip_capacity.go
    	modified:   cmd/network/subnet_ip_capacity_test.go
    	modified:   cmd/scalability/SpotNodeTerminationHandler.go
    	modified:   cmd/scalability/SpotNodeTerminationHandler_test.go
    	modified:   cmd/scalability/karpenter_nodegroup.go
    	modified:   cmd/scalability/karpenter_nodegroup_test.go
    	modified:   cmd/security/IRSAandPodIdentity.go
    	modified:   cmd/security/PVEcryption.go
    	modified:   cmd/security/PVEcryption_test.go
    	modified:   cmd/security/check_access_control.go
    	modified:   cmd/security/containe_root.go
    	modified:   cmd/security/minimum_iam_role.go
    	modified:   cmd/security/readonlyFilesystem.go
    	modified:   cmd/security/secretEncryption.go
    	modified:   cmd/security/secretEncryption_test.go
    	modified:   cmd/stability/AutoScaleNodeGroup.go
    	modified:   cmd/stability/AutoScaleNodeGroup_test.go
    	modified:   cmd/stability/CheckProbe.go
    	modified:   cmd/stability/CheckProbe_test.go
    	modified:   cmd/stability/CoreDnsCache.go
    	modified:   cmd/stability/CoreDnsCache_test.go
    	modified:   cmd/stability/DaemonSet-Priority-Class.go
    	modified:   cmd/stability/DaemonSet-Priority-Class_test.go
    	modified:   cmd/stability/Kapenter-Node.go
    	modified:   cmd/stability/Kapenter-Node_test.go
    	modified:   cmd/stability/Node-MultiAZ.go
    	modified:   cmd/stability/Node-MultiAZ_test.go
    	modified:   cmd/stability/singletone_pod.go
    	modified:   cmd/stability/singletone_pod_test.go
    	modified:   cmd/testutils/testutils.go
    	modified:   docs/network/test.md
    	modified:   go.mod
    	modified:   go.sum
    	modified:   testdata/karpenter_nodegroup.yaml
    	modified:   testdata/subnet_ip_capacity.yaml
    
    * 2차 커밋

--- cmd/general/check_image_tag_test.go
+++ cmd/general/check_image_tag_test.go
@@ -2,51 +2,43 @@ package general_test
 
 import (
 	"context"
-	"io/ioutil"
-	"path/filepath"
 	"testing"
 
-	"gopkg.in/yaml.v2"
-
 	general "eks-checklist/cmd/general"
+	"eks-checklist/cmd/testutils"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	fakekube "k8s.io/client-go/kubernetes/fake"
 )
 
-type TestCase struct {
-	Name          string   `yaml:"name"`
-	PodImages     []string `yaml:"pod_images"`
-	ExpectFailure bool     `yaml:"expect_failure"`
-}
-
-func loadTestCases(t *testing.T) []TestCase {
-	path, err := filepath.Abs(filepath.Join("..", "..", "testdata", "check_image_tag.yaml"))
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		t.Fatalf("failed to read test data: %v", err)
-	}
-
-	var cases []TestCase
-	if err := yaml.Unmarshal(data, &cases); err != nil {
-		t.Fatalf("failed to parse YAML: %v", err)
-	}
-
-	return cases
-}
-
 func TestCheckImageTag(t *testing.T) {
-	cases := loadTestCases(t)
+	// testutils에서 LoadTestCases를 사용하여 YAML 파일 로드
+	cases := testutils.LoadTestCases(t, "check_image_tag.yaml")
 
+	// 각 테스트 케이스에 대해 반복
 	for _, tc := range cases {
-		t.Run(tc.Name, func(t *testing.T) {
-			// Fake client 생성
+		// 'name' 필드를 가져와서 string 타입으로 처리
+		name := tc["name"].(string)
+
+		// 'expect_failure' 필드를 가져와서 bool 타입으로 처리
+		expectFailure := tc["expect_failure"].(bool)
+
+		// 'pod_images' 필드를 가져오고, []interface{}로 받은 후 []string으로 변환
+		rawPodImages := tc["pod_images"].([]interface{})
+		var podImages []string
+		for _, img := range rawPodImages {
+			podImages = append(podImages, img.(string))
+		}
+
+		// 테스트 실행
+		t.Run(name, func(t *testing.T) {
+			// Fake Kubernetes 클러스터 생성
 			client := fakekube.NewSimpleClientset()
 
 			// Fake Pod 생성
 			var podContainers []corev1.Container
-			for _, img := range tc.PodImages {
+			for _, img := range podImages {
 				podContainers = append(podContainers, corev1.Container{Image: img})
 			}
 
@@ -63,8 +55,8 @@ func TestCheckImageTag(t *testing.T) {
 
 			// CheckImageTag 함수 실행 및 결과 검증
 			result := general.CheckImageTag(client)
-			if result != tc.ExpectFailure {
-				t.Errorf("expected %v, got %v", tc.ExpectFailure, result)
+			if result != expectFailure {
+				t.Errorf("expected %v, got %v", expectFailure, result)
 			}
 		})
 	}
