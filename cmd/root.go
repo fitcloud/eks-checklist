7033c9f16bcf9e6154887b5b760adfce9ac7c0ba
0RC4 <wjdwldn1121@gmail.com>
2025-04-04 01:02:54 +0000

    GitOps 적용

--- cmd/root.go
+++ cmd/root.go
@@ -45,6 +45,9 @@ var rootCmd = &cobra.Command{
 		// 코드형 인프라 (EKS 클러스터, 애플리케이션 배포)
 		common.PrintResult(general.CheckIAC())
 
+		// GitOps 적용
+		common.PrintResult(general.CheckGitOps())
+
 		// 컨테이너 이미지 태그에 latest 미사용
 		common.PrintResult(general.CheckImageTag(k8sClient))
 
