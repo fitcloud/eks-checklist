7e50c471f4e3a7e47b2124495bd9b0f7dee9fe73
0RC4 <wjdwldn1121@gmail.com>
2025-04-04 02:18:45 +0000

    중요 워크로드에 대한 PDB(Pod Distruption Budget) 적용 - Automatic/Manual

---
+++ cmd/stability/pod_distruption_budget.go
@@ -0,0 +1,17 @@
+package stability
+
+import (
+	"eks-checklist/cmd/common"
+)
+
+func CheckPDB() common.CheckResult {
+	result := common.CheckResult{
+		CheckName:  "중요 워크로드에 대한 PDB(Pod Distruption Budget) 적용",
+		Manual:     true,
+		Passed:     false,
+		FailureMsg: "중요 워크로드 application은 PDB 설정을 통해 가용성을 지키는 것이 좋습니다",
+		Runbook:    "https://fitcloud.github.io/eks-checklist/컨테이너_이미지_태그에_latest_미사용/",
+	}
+
+	return result
+}
