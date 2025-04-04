c519f34aed936e57bf491ae3915cc89db147f00d
0RC4 <wjdwldn1121@gmail.com>
2025-04-04 02:11:40 +0000

    노드 확장/축소 정책 적용 - Manual

---
+++ cmd/scalability/nodeScalingPolicy.go
@@ -0,0 +1,17 @@
+package scalability
+
+import (
+	"eks-checklist/cmd/common"
+)
+
+func CheckNodeScalingPolicy() common.CheckResult {
+	result := common.CheckResult{
+		CheckName:  "노드 확장/축소 정책 적용 - Manual",
+		Manual:     true,
+		Passed:     false,
+		FailureMsg: "EKS Node는 AutoscaleGroup 또는 Karpenter Nodepool과 같은 동적 프로비저닝 하는 것이 좋습니다.",
+		Runbook:    "https://fitcloud.github.io/eks-checklist/컨테이너_이미지_태그에_latest_미사용/",
+	}
+
+	return result
+}
