29ffd7b89eaddec60f6f317e3d0aeb6b0a9a5c96
0RC4 <129482192+jwjoung@users.noreply.github.com>
2025-04-04 10:05:42 +0900

    GitOps 적용 (#72)

---
+++ cmd/general/gitops.go
@@ -0,0 +1,17 @@
+package general
+
+import (
+	"eks-checklist/cmd/common"
+)
+
+func CheckGitOps() common.CheckResult {
+	result := common.CheckResult{
+		CheckName:  "GitOps 적용",
+		Manual:     true,
+		Passed:     false,
+		FailureMsg: "Git 기반의 배포 도구를 사용하여 워크로드의 일관성을 유지하는것이 좋습니다 (ArgoCD, FluxCD,l Jenkins X …etc)",
+		Runbook:    "https://fitcloud.github.io/eks-checklist/컨테이너_이미지_태그에_latest_미사용/",
+	}
+
+	return result
+}
